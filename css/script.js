8a46d4833ae076cdf376a3961d12afa1908b3494
vincent <vincentmwitik@gmail.com>
2022-03-09 13:41:33 +0300

    add arrays

--- css/script.js
+++ css/script.js
@@ -1,3 +1,8 @@
 function findingNames(){
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
 
 }
