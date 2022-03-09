908de2967579b5ba0bb950ec550ab1a8e546da2a
vincent <vincentmwitik@gmail.com>
2022-03-09 13:42:04 +0300

    add date variables

--- css/script.js
+++ css/script.js
@@ -5,4 +5,12 @@ function findingNames(){
     var male = document.getElementById("male");
     var female = document.getElementById("female");
 
+    var DATE = document.getElementById("date");
+    dateArray = DATE.value.split("-");
+
+    var CC = parseInt(dateArray[0][0] + dateArray[0][1]);
+    var YY = parseInt(dateArray[0][2] + dateArray[0][3]);
+    var MM = parseInt(dateArray[1])
+    var DD = parseInt(dateArray[2])
+
 }
