323004560676523a9f69cba507705fa1c8f6b5d0
vincent <vincentmwitik@gmail.com>
2022-03-09 12:25:42 +0300

    feature will add a function findingNames

---
+++ css/script.js
@@ -0,0 +1,71 @@
+
+
+/* let button = document.getElementById("btn");
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    let myDate = new Date(formData.get("date"));
+    alert("hello")
+})
+ */
+
+
+// document.getElementById("btn").addEventListener("click", findingNames);
+
+
+function findingNames(){
+
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+
+    var DATE = document.getElementById("date");
+    dateArray = DATE.value.split("-");
+
+    var CC = parseInt(dateArray[0][0] + dateArray[0][1]);
+    var YY = parseInt(dateArray[0][2] + dateArray[0][3]);
+    var MM = parseInt(dateArray[1])
+    var DD = parseInt(dateArray[2])
+
+    day = parseInt((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
+
+    if (maleNames[day] === undefined || femaleNames[day] === undefined) {
+        alert("invalid date")
+
+    } 
+    else {
+        if (male.selected === true) {
+            console.log(maleNames[day])
+            alert("your name is " + maleNames[day])
+        }
+        if (female.selected === true) {
+            console.log(femaleNames[day])
+            alert("your name is" + femaleNames[day])
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
